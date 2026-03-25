1bdc1560a70296b80f73828fbff56f29d490e25f
surya-075 <suryatuty123@gmail.com>
2026-03-25 09:05:48 +0530

    UC1 PRINT OOPS To console

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+/**
+ * OOPSBannerApp
+ * * This class serves as the initial entry point for the OOPS Banner Application.
+ * Its primary goal in UC1 is to demonstrate basic Java execution by printing
+ * the literal string "OOPS" to the console.
+ * * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    /**
+     * The main method is the entry point of the Java application.
+     * * @param args Command line arguments (not used in this UC)
+     */
+    public static void main(String[] args) {
+        // Use System.out.println to display the String Literal "OOPS"
+        System.out.println("OOPS");
     }
 }
